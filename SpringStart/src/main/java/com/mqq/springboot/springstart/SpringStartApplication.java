77f49ca0b60dbb8ecca0fbff8219364b31af3c39
马齐齐 <maqiqi@topscomm.com>
2018-12-17 13:33:17 +0800

    调整

--- SpringStart/src/main/java/com/mqq/springboot/springstart/SpringStartApplication.java
+++ SpringStart/src/main/java/com/mqq/springboot/springstart/SpringStartApplication.java
@@ -1,12 +1,13 @@
-package com.mqq.springBoot.SpringStart;
+package com.mqq.springboot.springstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
+@ComponentScan("com.mqq.springboot.controller")
 public class SpringStartApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringStartApplication.class, args);
 	}
+
 }
